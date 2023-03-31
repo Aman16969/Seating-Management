c1c813f62a720122e47bd552e89e942f6f3f4e46
aman.ranjan <aman.ranjan@accolitedigital.com>
2023-03-31 15:15:43 +0530

    Added Application.propeties Please Change your database schema name Accordingly

--- src/main/java/com/example/SeatingManagement/Entity/Users.java
+++ src/main/java/com/example/SeatingManagement/Entity/Users.java
@@ -1,4 +1,32 @@
 package com.example.SeatingManagement.Entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.UUID;
+
+@Entity
+@Table(name="Users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Email(message = "Email should be of email type")
+    private String email;
+    private String name;
+    private String password;
+    private String designation;
+
+
 }
--- src/main/java/com/example/SeatingManagement/PayLoad/tset.java
+++
@@ -1,4 +0,0 @@
-package com.example.SeatingManagement.PayLoad;
-
-public class tset {
-}
