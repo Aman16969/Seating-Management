918a05c53098b076777ad09b8c4e5aa348ce8cdb
aman.ranjan <aman.ranjan@accolitedigital.com>
2023-03-31 14:36:21 +0530

    Setup

---
+++ src/test/java/com/example/SeatingManagement/SeatingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SeatingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
