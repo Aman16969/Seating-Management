e6e454b4b7f864103cd159da2ace905bd89c5ef2
nireeksha.jain <ss.jain@accolitedigital.com>
2023-05-04 10:25:13 +0530

    changes

--- src/main/java/com/example/SeatingManagement/EntityRequestBody/RequestBookingRoomDto.java
+++ src/main/java/com/example/SeatingManagement/EntityRequestBody/RequestBookingRoomDto.java
@@ -18,4 +18,8 @@ public class RequestBookingRoomDto {
     private String description;
     private boolean isActive=true;
     private boolean accepted=false;
+    private String date;
+    private String fromTime;
+    private String toTime;
+    private String roomType;
 }
